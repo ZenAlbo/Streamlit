b5c38c0fa9be1d35344bc625bd399e2f21824d49
Russel Albo <158298706+ZenAlbo@users.noreply.github.com>
2024-05-17 16:08:20 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
+from PIL import Image
+import numpy as np
+
+model = load_model('C:\Document\cifar10_model.h5')
+
+class_names = ['Airplane', 'Automobile', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
+
+st.title("CIFAR-10 Image Classification")
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image', use_column_width=True)
+    
+    
+    image = image.resize((32, 32))
+    image = img_to_array(image)
+    image = image.astype('float32') / 255.0
+    image = np.expand_dims(image, axis=0)
+    
+    prediction = model.predict(image)
+    predicted_class = class_names[np.argmax(prediction)]
+    
+    st.write(f'Predicted Class: (predicted_class)')
+
